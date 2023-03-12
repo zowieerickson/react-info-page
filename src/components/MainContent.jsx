5590816d541fc81a9839e76e065bb8ba16eea240
Zowie Erickson <zerickson@apple.com>
2023-03-11 21:55:04 -0500

    add mobile-first responsiveness

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,13 +1,13 @@
 function MainContent() {
     return (
         <main className="main">
-            <h1 className="main__title">Fun facts about React</h1>
+            <h1 className="main__title">Fun facts about&nbsp;React</h1>
             <ul className="main__list">
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on GitHub</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
+                <li>Was first released in&nbsp;2013</li>
+                <li>Was originally created by Jordan&nbsp;Walke</li>
+                <li>Has well over 100K stars on&nbsp;GitHub</li>
+                <li>Is maintained by&nbsp;Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile&nbsp;apps</li>
             </ul>
         </main>
     )
